067f38403832e47b72d1b14f5684b355fbe8bcae
JoseLRiano <joselriano@yahoo.com>
2020-04-02 04:41:34 -0400

    Updates

---
+++ precache-manifest.a2c1d83ab5802ea783ed6aba95e822fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8818362c1b311d89c46bee2ed2ec3bc2",
+    "url": "/ReactVideoPlayer/index.html"
+  },
+  {
+    "revision": "b8ab4e5956eea5238507",
+    "url": "/ReactVideoPlayer/static/css/main.5518eef6.chunk.css"
+  },
+  {
+    "revision": "f41fd6e7cea080df2932",
+    "url": "/ReactVideoPlayer/static/js/2.f7a079da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactVideoPlayer/static/js/2.f7a079da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8ab4e5956eea5238507",
+    "url": "/ReactVideoPlayer/static/js/main.63be2131.chunk.js"
+  },
+  {
+    "revision": "84df68b849acd0bd212d",
+    "url": "/ReactVideoPlayer/static/js/runtime-main.765eb9c2.js"
+  }
+]);
